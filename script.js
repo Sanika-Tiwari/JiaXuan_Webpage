7db29f77b808f55afd2bbe9c020f5bdfcb0b61d7
Sanika-Tiwari <thesanikatiwari@gmail.com>
2021-12-30 10:27:38 +0800

    Jia Xuan's Webpage

---
+++ script.js
@@ -0,0 +1,9 @@
+function SetBackgroundColor(){
+    document.getElementById("body").style.backgroundColor="black";
+  }
+  function changeImageHover(){
+    document.getElementById("JiaXuan").src="https://i.postimg.cc/MpCjCPrs/photo.jpg";
+  }
+  function changeImageLeave(){
+    document.getElementById("JiaXuan").src="https://i.postimg.cc/R04CwsNs/photo2.png";
+  }
